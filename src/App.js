e2b99a24fee14670622dbdf25d9e4c37236f1f3d
Scott Gibbons <William.s.gibbons.23@dartmouth.edu>
2020-04-23 16:25:40 -0400

    April 23 number 2

--- src/App.js
+++ src/App.js
@@ -5,24 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="all">
-    <div className="dartCalLogo">
-      DartCal
-      <div><img width="80px" src={logo}/></div>
-    </div>
-    <div className="displaySignInInfoContainer">
-      <div className="displaySignInInfo">
-        <div className="signInBox">
-          <div className="prompt"> Username: </div>
-            <input className="response" id="emailInputBar" placeholder="Username"  />
-          <div className="prompt"> Password: </div>
-            <input type="password" className="response" id="passwordInput" placeholder="Password"  />
-        </div>
-        <div>
+      <div className="dartCalLogo">
+        DartCal
+        <div><img width="80px" src={logo}/></div>
+      </div>
+      <div className="signInBox">
+        <div className="prompt"> Username: </div>
+        <input className="response" id="emailInputBar" placeholder="Username"  />
+      </div>
+      <div className="signInBox">
+        <div className="prompt"> Password: </div>
+        <input type="password" className="response" id="passwordInput" placeholder="Password"  />
+      </div>
+      <div>
         <button className="signupButtons" id="createButton" >Log In</button>
       </div>
     </div>
-  </div>
-  </div>
   );
 }
 
