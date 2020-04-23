6139b2d74da9964eee376c7425ea778d37320b0e
lilymaechling <lily.s.maechling.23@dartmouth.edu>
2020-04-23 09:26:25 -0700

    updated login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import logo from './calendar.png';
 import './App.css';
 
 function App() {
   return (
-    <div>
-        <div className="DartCal">
-          DartCal
-          
-        </div>
+    <div className="all">
+    <div className="dartCalLogo" style={{display: 'flex', flexDirection: 'row'}}>
+      DartCal
+      <div><img width="80px" src={logo}/></div>
     </div>
+    <div className="displaySignInInfoContainer">
+      <div className="displaySignInInfo">
+        <div className="leftJustify" style={{ width: '100%' }}>
+          <div className="prompt"> Username: </div>
+            <input className="response" id="emailInputBar" placeholder="Username"  />
+          <div className="prompt"> Password: </div>
+            <input type="password" className="response" id="passwordInput" placeholder="Password"  />
+        </div>
+        <div>
+        <button className="signupButtons" id="createButton" >Log In</button>
+      </div>
+    </div>
+  </div>
+  </div>
   );
 }
 
